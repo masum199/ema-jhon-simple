03b18f64fc9a8592cb3e6b8074e2e1b03bb318f1
selfish199 <24abdullahalmasum>
2023-04-05 17:09:22 +0600

    added remove peocess

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import LogIn from './components/LogIn/LogIn';
+import cartProductLoader from './Loader/cartProductsLoader';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Shop></Shop>
+      },
+      {
+        path:'orders',
+        element:<Orders></Orders>,
+        loader:cartProductLoader
+      },
+      {
+        path:'inventory',
+        element:<Inventory></Inventory>
+      },
+      {
+        path:'login',
+        element:<LogIn></LogIn>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
